efdf24418d6ba7d2b3ebe6ff574128e766ef72a4
Henrique Miranda <enrque.ns@gmail.com>
2020-08-20 11:30:45 -0300

    :sparkles: feat: create a analytics client

--- node/clients/analytics.ts
+++ node/clients/analytics.ts
@@ -1,3 +1,18 @@
-import { AppClient } from '@vtex/api'
+import { AppClient, InstanceOptions, IOContext } from '@vtex/api'
 
-export default class Analytics extends AppClient {}
+interface LiveUsersProduct {
+  slug: string;
+  liveUsers: number;
+}
+
+class Analytics extends AppClient {
+  constructor(context: IOContext, options?: InstanceOptions) {
+    super("vtex.mocked-analytics@0.x", context, options);
+  }
+
+  public getLiveUsers(): Promise<LiveUsersProduct[]> {
+    return this.http.get('_v/live-products')
+  }
+}
+
+export default Analytics
