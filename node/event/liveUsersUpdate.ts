d4f73e0ea8838f2307f6148fb63039ea9582ff42
Henrique Miranda <enrque.ns@gmail.com>
2020-08-20 11:43:07 -0300

    :sparkles: feat: use events as triggers

--- node/event/liveUsersUpdate.ts
+++ node/event/liveUsersUpdate.ts
@@ -1,3 +1,10 @@
-export async function updateLiveUsers() {
-  console.log('EVENT HANDLER: received event')
+import { EventContext } from '@vtex/api'
+
+import { Clients } from '../clients/index'
+
+export async function updateLiveUsers(ctx: EventContext<Clients>) {
+  const liveUsersProducts = await ctx.clients.analytics.getLiveUsers()
+  console.log('LIVE USERS: ', liveUsersProducts)
+
+  return true
 } 
--- node/handlers/analytics.ts
+++ node/handlers/analytics.ts
@@ -3,11 +3,10 @@ export async function analytics(ctx: Context, next: () => Promise<any>) {
     const { clients: { analytics } } = ctx 
 
     ctx.status = 200
-    ctx.body = 'OK'
-
     ctx.body = await analytics.getLiveUsers() 
+    
     ctx.set ('cache-control', 'no-cache')
   }
-  
+
   await next()
 }
