23848aaeff5c5f48979c0120640641b4e0630e11
Henrique Miranda <enrque.ns@gmail.com>
2020-08-20 22:28:38 -0300

    :sparkles: feat: use master data

--- node/event/liveUsersUpdate.ts
+++ node/event/liveUsersUpdate.ts
@@ -1,10 +1,50 @@
-import { EventContext } from '@vtex/api'
+import { EventContext } from "@vtex/api"
 
-import { Clients } from '../clients/index'
+import { Clients } from "../clients/index"
+import { COURSE_ENTITY } from "../utils/constants"
+
+interface SearchDocumentDTO {
+  id: string;
+  count: number;
+  slug: string;
+}
 
 export async function updateLiveUsers(ctx: EventContext<Clients>) {
   const liveUsersProducts = await ctx.clients.analytics.getLiveUsers()
-  console.log('LIVE USERS: ', liveUsersProducts)
+  console.log("LIVE USERS: ", liveUsersProducts)
+
+  await Promise.all(
+    liveUsersProducts.map(async ({ slug, liveUsers }) => {
+      try {
+        const [savedProduct] = await ctx.clients.masterdata.searchDocuments<SearchDocumentDTO>({
+          dataEntity: COURSE_ENTITY,
+          fields: ["count", "id", "slug"],
+          pagination: {
+            page: 1,
+            pageSize: 1,
+          },
+          schema: "v1",
+          where: `slug=${slug}`,
+        })
+  
+        console.log("SAVED PRODUCT", savedProduct)
+
+        await ctx.clients.masterdata.createOrUpdateEntireDocument({
+          dataEntity: COURSE_ENTITY,
+          fields: {
+            count: liveUsers,
+            slug,
+          },
+          id: savedProduct?.id,
+        })
+
+        
+      } catch (error) {
+        console.log(`failed to update product ${slug}`)
+        console.log(error)
+      }
+    })
+  )
 
   return true
-} 
+}
